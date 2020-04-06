9d794d40c2f7440f632e07ad02b773a3524190ba
Mariela Cruz <heimys>
2020-04-06 00:13:33 -0400

    Implement witdraw method with default balance

--- src/main/java/Bank/BankApp.java
+++ src/main/java/Bank/BankApp.java
@@ -3,6 +3,9 @@ package Bank;
 import java.util.Scanner;
 
 public class BankApp {
+    private int balance = 100;
+    private Scanner scanner = new Scanner(System.in);
+
     public void initSession(String name, String account) {
         printWelcome(name, account);
         printMenu();
@@ -22,38 +25,36 @@ public class BankApp {
     }
 
     private void getClientChoice() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            char option;
-            do {
-                System.out.print("Select an option: ");
-                option = (char) scanner.nextLine().trim().toUpperCase().charAt(0);
-                switch (option) {
-                    case 'W':
-                        withdraw();
-                        break;
-                    case 'D':
-                        deposit();
-                        break;
-                    case 'B':
-                        showBalance();
-                        break;
-                    case 'T':
-                        showTransactions();
-                        break;
-                    case 'A':
-                        printMenu();
-                        break;
-                    case 'E':
-                        endSession();
-                        break;
-                    default:
-                        println(option + " is an invalid option");
-                        break;
-                }
-                println(System.lineSeparator());
-                println("(A) Show All options");
-            } while (option != 'E');
-        }
+        char option;
+        do {
+            System.out.print("What do you want to do? Select an option: ");
+            option = (char) scanner.next().trim().toUpperCase().charAt(0);
+            switch (option) {
+                case 'W':
+                    withdraw();
+                    break;
+                case 'D':
+                    deposit();
+                    break;
+                case 'B':
+                    showBalance();
+                    break;
+                case 'T':
+                    showTransactions();
+                    break;
+                case 'A':
+                    printMenu();
+                    break;
+                case 'E':
+                    endSession();
+                    break;
+                default:
+                    println(option + " is an invalid option");
+                    break;
+            }
+            println(System.lineSeparator());
+            println("(A) Show All options");
+        } while (option != 'E');
     }
 
     private void endSession() {
@@ -65,7 +66,7 @@ public class BankApp {
     }
 
     private void showBalance() {
-        println("My Balance...");
+        println("Your Balance is: " + balance);
     }
 
     private void deposit() {
@@ -73,7 +74,18 @@ public class BankApp {
     }
 
     private void withdraw() {
-        println("Withdraw...");
+        System.out.print("Amount to withdraw: ");
+
+        int amount = scanner.nextInt();
+
+        if (amount <= balance) {
+            balance = balance - amount;
+            println("Withdraw complete.");
+            showBalance();
+        }
+        else {
+            println("Not enough funds. ");
+        }
     }
 
     private void printWelcome(String name, String account) {
